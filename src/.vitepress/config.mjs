d296a4b8486d2d2c61e27b889aff17444801e59b
Alexey Shmelev <alexsey_89@bk.ru>
2026-01-08 19:41:26 +0300

    fixed config

--- src/.vitepress/config.mjs
+++ src/.vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  //base: '/docs/',
+  base: '/docs/',
   title: "Docs",
   lang: 'ru-RU',
   description: "Modern documentation",
